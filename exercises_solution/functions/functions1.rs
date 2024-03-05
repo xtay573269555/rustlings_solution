c936b090e7506ee0d8b23b9670c8f597ea801fad
xtay573269555 <xtay573269555@gmail.com>
2024-03-05 14:28:22 +0800

    add solution

---
+++ exercises_solution/functions/functions1.rs
@@ -0,0 +1,12 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+fn call_me() {
+    println!("aaa");
+}
+
+fn main() {
+    call_me();
+}
